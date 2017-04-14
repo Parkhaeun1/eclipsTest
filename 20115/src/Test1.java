871ef01b3ea069bc2ebcb61fce796637ead4fa60
user <user@edi15>
2017-04-14 14:18:51 +0900

    추가2

--- 20115/src/Test1.java
+++ 20115/src/Test1.java
@@ -4,6 +4,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("박하은");
 		System.out.println("박유순");
+		System.out.println("박하영");
+		System.out.println("룰루랄라");
 
 	}
 
